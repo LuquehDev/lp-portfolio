228b652d5dfb50322ad63afe478eccb716aea5d6
LuquehDev <paivaluizhenrique.lh@gmail.com>
2025-05-13 18:00:03 +0100

    1.0.1.1.0

--- components/components/topscroll-component.tsx
+++ components/components/topscroll-component.tsx
@@ -1,18 +1,27 @@
 "use client";
 
 import { ChevronUp } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function ScrollToTopButton() {
   const [isVisible, setIsVisible] = useState(false);
+  const [offsetFromFooter, setOffsetFromFooter] = useState(false);
+  const footerRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
-    const toggleVisibility = () => {
+    footerRef.current = document.querySelector("footer");
+    const handleScroll = () => {
       setIsVisible(window.scrollY > 300);
+
+      if (footerRef.current) {
+        const footerTop = footerRef.current.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        setOffsetFromFooter(footerTop < windowHeight + 10);
+      }
     };
 
-    window.addEventListener("scroll", toggleVisibility);
-    return () => window.removeEventListener("scroll", toggleVisibility);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   const scrollToTop = () => {
@@ -22,9 +31,12 @@ export default function ScrollToTopButton() {
   return (
     <button
       onClick={scrollToTop}
-      className={`fixed bottom-6 right-6 z-50 w-12 h-12 rounded-full bg-muted/30 backdrop-blur-sm text-primary cursor-pointer shadow-md hover:bg-muted/50 transition-opacity duration-300 ${
+      className={`fixed right-6 z-50 w-12 h-12 rounded-full bg-muted/30 backdrop-blur-sm text-primary cursor-pointer shadow-md hover:bg-muted/50 transition-all duration-300 ${
         isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
-      }`}
+      } ${offsetFromFooter ? "mb-6" : ""}`}
+      style={{
+        bottom: offsetFromFooter ? "90px" : "24px", // sobe quando encostar o footer
+      }}
     >
       <ChevronUp className="mx-auto" />
     </button>
